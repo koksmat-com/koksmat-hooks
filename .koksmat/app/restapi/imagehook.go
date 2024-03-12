fa822db873a8e125478918367afea87157eaebf8
Niels Gregers Johansen <niels@timeboxer.dk>
2024-03-12 08:01:26 +0100

    added imagehook

--- .koksmat/app/restapi/imagehook.go
+++ .koksmat/app/restapi/imagehook.go
@@ -2,24 +2,27 @@ package restapi
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func ImageHook(w http.ResponseWriter, r *http.Request) {
 
 	id := r.URL.Query().Get("id")
-	etag := r.URL.Query().Get("etag")
+	version := r.URL.Query().Get("version")
 	listname := r.URL.Query().Get("listname")
 	sitename := r.URL.Query().Get("sitename")
-	tenantname := r.URL.Query().Get("tenant")
+	tenantname := r.URL.Query().Get("tenantname")
 
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(200)
 	fmt.Fprint(w, " id:", id)
-	fmt.Fprint(w, " etag:", etag)
-	fmt.Fprint(w, " listname", listname)
-	fmt.Fprint(w, " sitename", sitename)
-	fmt.Fprint(w, " tenantname", tenantname)
+	fmt.Fprint(w, " version: ", version)
+	fmt.Fprint(w, " listname: ", listname)
+	fmt.Fprint(w, " sitename: ", sitename)
+	fmt.Fprint(w, " tenantname: ", tenantname)
+
+	log.Println("ImageHook called", id, version, listname, sitename, tenantname)
 
 }
--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@ description: Describe the main purpose of this kitchen
 ---
 
 # koksmat-hooks
+
+http://localhost:8080/imagehook?id=1&version=2&listname=3&sitename=4&tenantname=5
+
