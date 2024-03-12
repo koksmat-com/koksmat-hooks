cbad562b48af8cafcd94ec6e2ae234b4ab947dc0
Niels Gregers Johansen <niels@timeboxer.dk>
2024-03-12 12:44:24 +0100

    smaller app image

---
+++ 60-provision/10-app-serve.ps1
@@ -0,0 +1,98 @@
+<#---
+title: App deploy to production
+tag: appdeployproduction
+api: post
+---
+We start by finding which version tag to use
+
+eventually 
+
+nexi-booking provision webdeployproduction 
+#>
+
+$appname = "koksmat-hooks"
+$imagename = "koksmat-hooks"
+$dnsname = "koksmat-hooks.home.nexi-intra.com"
+$org = "koksmat-com"
+$inputFile = join-path  $env:KITCHENROOT $appname ".koksmat","koksmat.json"
+$port = 8080
+if (!(Test-Path -Path $inputFile) ) {
+   Throw "Cannot find file at expected path: $inputFile"
+} 
+$json = Get-Content -Path $inputFile | ConvertFrom-Json
+$version = "v$($json.version.major).$($json.version.minor).$($json.version.patch).$($json.version.build)"
+
+<#
+The we build the deployment file
+#>
+
+$image = "ghcr.io/$org/$($imagename)-app:$($version)"
+
+$config = @"
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: $appname-app
+spec:
+  selector:
+    matchLabels:
+      app: $appname-app
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: $appname-app
+    spec: 
+      containers:
+      - name: $appname-app
+        image: $image
+        command: [$appname]
+        args: ["serve"]               
+        ports:
+          - containerPort: $port
+        env:
+        - name: NAME
+          value: VALUE
+    
+        
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: $appname-app
+  labels:
+    app: $appname-app
+    service: $appname-app
+spec:
+  ports:
+  - name: http
+    port: 8080
+    targetPort: $port
+  selector:
+    app: $appname
+---    
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: $appname
+spec:
+  rules:
+  - host: $dnsname
+    http:
+      paths:
+      - path: /
+        pathType: Prefix
+        backend:
+          service:
+            name: $appname-app
+            port:
+              number: 8080
+    
+
+"@
+
+write-host "Applying config" -ForegroundColor Green
+
+write-host $config -ForegroundColor Gray
+
+$config |  kubectl apply -f -
--- Dockerfile.app
+++ Dockerfile.app
@@ -1,15 +1,8 @@
-FROM mcr.microsoft.com/azure-cli
-
-RUN apk update
-RUN apk add --upgrade powershell   
-RUN apk add go
+FROM golang:1.21
 
 WORKDIR /koksmat
 COPY . .
 WORKDIR /koksmat/.koksmat/app
 RUN go install
-
-
-
 
 CMD [ "sleep","infinity"]
